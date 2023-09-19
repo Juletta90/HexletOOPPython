f21087c17e5b26dd1a66826aa4fa547d614ff6a1
Juletta90 <57813598+Juletta90@users.noreply.github.com>
2023-09-19 17:38:58 +0300

    Add files via upload

--- lesson4.py
+++ lesson4.py
@@ -19,43 +19,82 @@ from points import get_x, get_y, make_decart_point
 # point = make_point(3, 4)
 # symmetrical_point = make_point(-get_x(point), get_y(point))
 # print(symmetrical_point)  # напечатает в виде словаря
+
+# point = make_decart_point(3, 4)
+# begin_point = make_decart_point(get_x(point), get_y(point))
+# end_point = make_decart_point(get_x(point), get_y(point))
 #
 # point = make_point(5, 7)  # мы не знаем как устроена точка
 # print(get_x(point))       # мы получаем доступ к частям только через селекторы
 # print(get_y(point))       # напечатает построчно
-# #____________________________#
+#
+#
+# # def get_middle_point(p1, p2):
+# #     x1, y1 = p1
+# #     x2, y2 = p2
+# #     x = (x1 + x2) / 2
+# #     y = (y1 + y2) / 2
+# #     return x, y
+# #
+# # point1 = 2, 3
+# # point2 = -4, 1
+# # get_middle_point(point1, point2)
+#
+#
+# # def make_segment(p1, p2):
+# #      [x1, y1], [x2, y2] = p1, p2
+# #      return segment(p1, p2)
+# #
+# # make_segment([15, 7], [22, 11])
+# #__________________________________________________________________________#
+
 
 # begin_point = make_decart_point(4, 2)
 # end_point = make_decart_point(0, 0)
 # segment = make_segment(begin_point, end_point)
 
-def make_segment(p1, p2):
-     x1, y1 = p1.values()
-     x2, y2 = p2.values()
-     begin_point = make_decart_point(x1, y1)
-     end_point = make_decart_point(x2, y2)
-     segment = make_segment(make_decart_point(x1, y1), make_decart_point(x2, y2))
-     print(begin_point, end_point)
+
+
+def get_mid_point_of_segment(begin_point, end_point):
+     x1, y1 = begin_point
+     x2, y2 = end_point
+     x = (x1 + x2) / 2
+     y = (y1 + y2) / 2
+     return x, y
+
+
+point1 = 2, 3
+point2 = -4, 1
+get_mid_point_of_segment(point1, point2)
+
+
+def make_segment(x, y):
+     begin_point = make_decart_point(x, y)
+     end_point = make_decart_point(x, y)
      return begin_point, end_point
 
-    # segment(make_decart_point(x1, y1), make_decart_point(x2, y2))
-
-make_segment(begin_point, end_point)
-
-
+point1 = 2, 3
+point2 = -4, 1
+segment = make_segment(point1, point2)
 
 
+def get_begin_point(segment):
+     begin_point = make_decart_point(get_x(point), get_y(point))    #make_decart_point(get_x(point), get_y(point))
+     return begin_point
 
 
+point = 2, 3
+get_begin_point(point)
 
 
+def get_end_point(segment):
+     end_point = make_decart_point(x, 0)
+     return end_point
 
-# def make_segment(x, y):
-#      begin_point = make_decart_point(x, y)
-#      end_point = make_decart_point(x, y)
-#      return begin_point, end_point
-#
-# # segment = make_segment(begin_point, end_point)
+
+get_end_point(segment)
+
+
 #
 #
 # point = make_decart_point(3, 4)
